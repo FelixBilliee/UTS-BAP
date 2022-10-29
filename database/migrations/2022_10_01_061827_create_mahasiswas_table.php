8b2acb62ca4e74425f2a4100118b8900e46c31d2
FelixBilliee <felixxbillie@gmail.com>
2022-10-29 15:29:59 +0700

    upload

---
+++ database/migrations/2022_10_01_061827_create_mahasiswas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string("studentID",10);
+            $table->string("nama",50);
+            $table->string("jurusan");
+            $table->string("tahunmasuk",4);
+            $table->string("Jenis_Kelamin",1);
+            $table->string("Alamat");
+            $table->string("Tanggal_Lahir");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
